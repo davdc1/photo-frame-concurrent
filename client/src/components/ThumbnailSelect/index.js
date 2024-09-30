a3abbc7856ccf21a04a51be35cdf88e7daffe12d
david cohen <davdc1@gmail.com>
2024-09-30 03:52:48 +0300

    update

--- client/src/components/ThumbnailSelect/index.js
+++ client/src/components/ThumbnailSelect/index.js
@@ -11,7 +11,7 @@ const ThumbnailSelect = ({ params: { item, onSelectItem, selected, deleteLoading
         toggle({ popupType: 'PhotoPrev', payload: { id: item.id } })
     }
 
-    return (
+    return 1 ? (
         <div
             className={`thumbnail-select-wrapper ${deleteLoading ? 'disabled' : ''}`}
             data-photoid={item.id}
@@ -24,6 +24,27 @@ const ThumbnailSelect = ({ params: { item, onSelectItem, selected, deleteLoading
             <span className='thumbnail-select-text' >{item.name_user}</span>
             <div className={`thumbnail-select-check ${selected[item.id] ? 'selected' : ''}`}></div>
         </div>
+    ) : (
+        // <div className='thumbnail-wrapper' onClick={prevPopup}>
+        //     <div className='thumbnail-img-container'>
+        //         <img className='thumbnail-img' src={item.url} alt='' />
+        //     </div>
+        //     <span className='thumbnail-text' >{item.name_user}</span>
+        // </div>
+
+
+        // <div
+        //     className={`thumbnail-select-wrapper ${deleteLoading ? 'disabled' : ''}`}
+        //     data-photoid={item.id}
+        //     onClick={deleteLoading ? null : onSelectItem}
+        // >
+        //     {1 ? <button onClick={prevPopup}>{"OPEN"}</button> : '' }
+        //     <div className='thumbnail-select-img-container'>
+        //         <img className='thumbnail-select-img' src={item.url} alt='' />
+        //     </div>
+        //     <span className='thumbnail-select-text' >{item.name_user}</span>
+        //     {1 ?  <div className={`thumbnail-select-check ${selected[item.id] ? 'selected' : ''}`}></div> : ''}
+        // </div>
     )
 }
 
